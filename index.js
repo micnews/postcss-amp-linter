17afb68de37688b3a4205b083be49b2e81faa289
David Björklund <david.bjorklund@gmail.com>
2016-02-11 23:51:47 +0100

    pseudo

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 import postcss from 'postcss';
 import selectorParser from 'postcss-selector-parser';
+import startsWith from 'lodash.startswith';
 
 const validateRule = (rule, result) => {
+  const validatePseudo = nodes => {
+    let pseudo = false;
+    let ampTagname = false;
+    let noneTag = false;
+
+    nodes.forEach(({type, value}) => {
+      pseudo = pseudo || type === 'pseudo';
+      ampTagname = ampTagname || type === 'tag' && startsWith(value, 'amp-');
+      noneTag = noneTag || type !== 'pseudo' && type !== 'tag' && type !== 'combinator';
+    });
+
+    if (pseudo && noneTag) {
+      rule.warn(result, 'Pseudo-selectors, pseudo-classes and pseudo-elements are only allowed in tag names.');
+    }
+
+    if (pseudo && ampTagname) {
+      rule.warn(result, 'Pseudo-selectors, pseudo-classes and pseudo-elements are not allowed in amp-tags.');
+    }
+  };
+
   const validateSelector = selector => {
     if (selector.type === 'universal') {
       rule.warn(result, 'The universal selector (*) is not allowed in AMP.');
@@ -9,6 +30,10 @@ const validateRule = (rule, result) => {
 
     if (selector.type === 'pseudo' && selector.value === ':not') {
       rule.warn(result, ':not() is not allowed in AMP.');
+    }
+
+    if (selector.type === 'selector') {
+      validatePseudo(selector.nodes);
     }
   };
 
--- test.js
+++ test.js
@@ -18,11 +18,31 @@ test('universal selector', t => {
 });
 
 test(':not() selector', t => {
-  const input = '.foo:not(.bar) { border: 1px}';
+  const input = 'div:not(span) { border: 1px}';
   const expectedText = ':not() is not allowed in AMP.';
   return process(input)
     .then(actual => {
       t.is(actual.warnings().length, 1);
       t.is(actual.warnings()[0].text, expectedText);
+    });
+});
+
+test('pseudo-selectors, pseudo-classes and pseudo-elements (allowed)', t => {
+  // TODO: test this more
+
+  const input = 'a:hover, div:last-of-type, p:hover a { border: 1px }';
+  return process(input)
+    .then(actual => {
+      t.is(actual.warnings().length, 0);
+    });
+});
+
+test('pseudo-selectors, pseudo-classes and pseudo-elements (disallowed)', t => {
+  // TODO: test this more
+
+  const input = 'amp-img:hover, amp-img:last-of-type, amp-img img:hover, div:hover amp-img, .foo:hover { border: 1px }';
+  return process(input)
+    .then(actual => {
+      t.is(actual.warnings().length, 5);
     });
 });
